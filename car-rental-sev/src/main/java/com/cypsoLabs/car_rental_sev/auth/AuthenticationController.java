656ba83cff7f10ceb9226ee75e4e8f9d0c5bacc7
Dulmika <semaldulmika@gmail.com>
2024-06-10 13:38:53 +0530

    implement global exception handler

--- car-rental-sev/src/main/java/com/cypsoLabs/car_rental_sev/auth/AuthenticationController.java
+++ car-rental-sev/src/main/java/com/cypsoLabs/car_rental_sev/auth/AuthenticationController.java
@@ -1,6 +1,6 @@
 package com.cypsoLabs.car_rental_sev.auth;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
+import  io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
--- car-rental-sev/src/main/java/com/cypsoLabs/car_rental_sev/auth/RegisterRequest.java
+++ car-rental-sev/src/main/java/com/cypsoLabs/car_rental_sev/auth/RegisterRequest.java
@@ -11,20 +11,19 @@ import lombok.Setter;
 public class RegisterRequest {
     @NotEmpty(message = "Firstname is mandatory")
     @NotNull(message = "Firstname is mandatory")
-    @NotBlank(message = "Firstname is mandatory")
     private String firstName;
 
     @NotEmpty(message = "Lastname is mandatory")
-    @NotBlank(message = "Lastname is mandatory")
+    @NotNull(message = "Lastname is mandatory")
     private String lastName;
 
-    @Email(message = "Email is not formatted")
+    @Email(message = "Email is not well formatted")
     @NotEmpty(message = "Email is mandatory")
-    @NotBlank(message = "Email is mandatory")
+    @NotNull(message = "Email is mandatory")
     private String email;
 
     @NotEmpty(message = "Password is mandatory")
-    @NotBlank(message = "Password is mandatory")
+    @NotNull(message = "Password is mandatory")
     @Size(min = 8, message = "Password should be 8 characters long minimum")
         private String password;
 }
